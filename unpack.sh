e57c19c6bc5f6226e81350d90bfcc7b4d970d432
Jakub Zeifert <kuba1102@gmail.com>
2024-10-22 15:53:27 +0200

    Update file paths in unpack.sh script

--- unpack.sh
+++ unpack.sh
@@ -3,11 +3,11 @@
 # Copy all config files from Repo back to their original locations
 
 #   i3
-cp /i3/config ~/.config/i3/
+cp -i ./i3/config ~/.config/i3/
 #   i3status
-cp /i3status/config ~/.config/i3status/
+cp -i ./i3status/config ~/.config/i3status/
 #   Picom
-cp /picom/picom.conf ~/.config/picom/
+cp -i ./picom/picom.conf ~/.config/picom/
 
 cp -r ./.icons ~/
 cp -r ./.fonts ~/
