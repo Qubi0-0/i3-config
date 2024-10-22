7bf510d05102f6a1d17a09fe3a0f410ffba6d8f7
Jakub Zeifert <kuba1102@gmail.com>
2024-10-22 15:52:09 +0200

    Update file paths in pack.sh script

--- pack.sh
+++ pack.sh
@@ -3,11 +3,11 @@
 # Copy all config files to Repo
 
 #   i3
-cp -i ~/.config/i3/config /i3/
+cp -i ~/.config/i3/config ./i3/
 #   i3status
-cp -i ~/.config/i3status/config /i3status/
+cp -i ~/.config/i3status/config ./i3status/
 #   Picom
-cp -i ~/.config/picom/picom.conf /picom/
+cp -i ~/.config/picom/picom.conf ./picom/
 
-cp -i -r ~/.icons .
-cp -i -r ~/.fonts .
+cp -r ~/.icons .
+cp -r ~/.fonts .
