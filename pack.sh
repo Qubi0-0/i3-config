4acd40f76be4d891668dff1bc7737223ac0b67a0
Jakub Zeifert <kuba1102@gmail.com>
2024-10-26 21:34:45 +0200

    Update file paths in pack.sh and unpack.sh scripts

--- pack.sh
+++ pack.sh
@@ -8,6 +8,9 @@ cp -i ~/.config/i3/config ./i3/
 cp -i ~/.config/i3status/config ./i3status/
 #   Picom
 cp -i ~/.config/picom/picom.conf ./picom/
+# Wallpapers
+cp -ir ~/Pictures/Wallpapers/nord/ .
+
 
 cp -r ~/.icons .
 cp -r ~/.fonts .
--- unpack.sh
+++ unpack.sh
@@ -8,6 +8,8 @@ cp -i ./i3/config ~/.config/i3/
 cp -i ./i3status/config ~/.config/i3status/
 #   Picom
 cp -i ./picom/picom.conf ~/.config/picom/
+# Wallpapers
+cp -ir nord/ ~/Pictures/Wallpapers/
 
 cp -r ./.icons ~/
 cp -r ./.fonts ~/
